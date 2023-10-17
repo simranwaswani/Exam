1a3aaec180915761c3084cec509234f2c70e9129
shoba rohra <130898772+simranwaswani@users.noreply.github.com>
2023-10-17 11:11:39 +0500

    integrate dropdown filter for city and occupation

--- Mid-Exam/src/Components/SampleTable.jsx
+++ Mid-Exam/src/Components/SampleTable.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 const SampleTable = ({ data }) => {
   const [filterName, setFilterName] = useState('')
   const [filterAge, setFilterAge] = useState('')
+  const [filterCity, setFilterCity] = useState('')
+  const [filterOccupation, setFilterOccupation] = useState('')
 
   const filteredData = data.filter((item) => {
     const nameMatch = item.name.toLowerCase().includes(filterName.toLowerCase())
@@ -32,7 +34,43 @@ const SampleTable = ({ data }) => {
           onChange={(e) => setFilterAge(e.target.value)}
         />
       </div>
-
+      <div>
+        <label>City: </label>
+        <select
+          value={filterCity}
+          onChange={(e) => setFilterCity(e.target.value)}
+        >
+          <option value=''>All</option>
+          <option value='New York'>New York</option>
+          <option value='Los Angeles'>Los Angeles</option>
+          <option value='Chicago'>Chicago</option>
+          <option value='San Francisco'>San Francisco</option>
+          <option value='Houston'>Houston</option>
+          <option value='Seattle'>Seattle</option>
+          <option value='Boston'>Boston</option>
+          <option value='Denver'>Denver</option>
+          <option value='Atlanta'>Atlanta</option>
+          <option value='Miami'>Miami</option>
+        </select>
+      </div>
+      <div>
+        <label>Occupation: </label>
+        <select
+          value={filterOccupation}
+          onChange={(e) => setFilterOccupation(e.target.value)}
+        >
+          <option value=''>All</option>
+          <option value='Engineer'>Engineer</option>
+          <option value='Teacher'>Teacher</option>
+          <option value='Designer'>Designer</option>
+          <option value='Accountant'>Accountant</option>
+          <option value='Doctor'>Doctor</option>
+          <option value='Software Engineer'>Software Engineer</option>
+          <option value='Lawyer'>Lawyer</option>
+          <option value='Marketing Manager'>Marketing Manager</option>
+          <option value='Entrepreneur'>Entrepreneur</option>
+        </select>
+      </div>
       <table>
         <thead>
           <tr>
@@ -49,6 +87,8 @@ const SampleTable = ({ data }) => {
               <td>{item.id}</td>
               <td>{item.name}</td>
               <td>{item.age}</td>
+              <td>{item.city}</td>
+              <td>{item.occupation}</td>
             </tr>
           ))}
         </tbody>
