49c425119260544c746457fca4b333740f309331
shoba rohra <130898772+simranwaswani@users.noreply.github.com>
2023-10-17 11:36:23 +0500

    add button to reset filter

--- Mid-Exam/src/Components/SampleTable.jsx
+++ Mid-Exam/src/Components/SampleTable.jsx
@@ -6,31 +6,51 @@ const SampleTable = ({ data }) => {
   const [filterCity, setFilterCity] = useState('')
   const [filterOccupation, setFilterOccupation] = useState('')
 
+  // State variables for managing data and filtered data
+  const [originalData, setOriginalData] = useState(data)
+  const [filteredData, setFilteredData] = useState(data)
+
   const handleFilterNameChange = (e) => {
     setFilterName(e.target.value)
+    filterData(e.target.value, filterAge, filterCity, filterOccupation)
   }
 
   const handleFilterAgeChange = (e) => {
     setFilterAge(e.target.value)
+    filterData(filterName, e.target.value, filterCity, filterOccupation)
   }
 
   const handleFilterCityChange = (e) => {
     setFilterCity(e.target.value)
+    filterData(filterName, filterAge, e.target.value, filterOccupation)
   }
 
   const handleFilterOccupationChange = (e) => {
     setFilterOccupation(e.target.value)
+    filterData(filterName, filterAge, filterCity, e.target.value)
   }
 
-  const filteredData = data.filter((item) => {
-    const nameMatch = item.name.toLowerCase().includes(filterName.toLowerCase())
-    const ageMatch = filterAge === '' || item.age === parseInt(filterAge)
-    const cityMatch = filterCity === '' || item.city === filterCity
-    const occupationMatch =
-      filterOccupation === '' || item.occupation === filterOccupation
+  const filterData = (name, age, city, occupation) => {
+    const filtered = originalData.filter((item) => {
+      const nameMatch = item.name.toLowerCase().includes(name.toLowerCase())
+      const ageMatch = age === '' || item.age === parseInt(age)
+      const cityMatch = city === '' || item.city === city
+      const occupationMatch =
+        occupation === '' || item.occupation === occupation
 
-    return nameMatch && ageMatch && cityMatch && occupationMatch
-  })
+      return nameMatch && ageMatch && cityMatch && occupationMatch
+    })
+
+    setFilteredData(filtered)
+  }
+
+  const resetFilters = () => {
+    setFilterName('')
+    setFilterAge('')
+    setFilterCity('')
+    setFilterOccupation('')
+    setFilteredData(originalData)
+  }
 
   return (
     <div>
@@ -56,6 +76,14 @@ const SampleTable = ({ data }) => {
           <option value=''>All</option>
           <option value='New York'>New York</option>
           <option value='Los Angeles'>Los Angeles</option>
+          <option value='San Francisco'>San Francisco</option>
+          <option value='Chicago'>Chicago</option>
+          <option value='Houston'>Houston</option>
+          <option value='Miami'>Miami</option>
+          <option value='Seattle'>Seattle</option>
+          <option value='Boston'>Boston</option>
+          <option value='Denver'>Denver</option>
+          <option value='Atlanta'>Atlanta</option>
         </select>
       </div>
       <div>
@@ -67,29 +95,40 @@ const SampleTable = ({ data }) => {
           <option value=''>All</option>
           <option value='Engineer'>Engineer</option>
           <option value='Teacher'>Teacher</option>
+          <option value='Designer'>Designer</option>
+          <option value='Accountant'>Accountant</option>
+          <option value='Doctor'>Doctor</option>
+          <option value='Artist'>Artist</option>
+          <option value='Software Engineer'>Software Engineer</option>
+          <option value='Lawyer'>Lawyer</option>
+          <option value='Marketing Manager'>Marketing Manager</option>
+          <option value='Entrepreneur'>Entrepreneur</option>
         </select>
       </div>
+      <button onClick={resetFilters}>Reset Filters</button>
       <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>City</th>
-            <th>Occupation</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.age}</td>
-              <td>{item.city}</td>
-              <td>{item.occupation}</td>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>City</th>
+              <th>Occupation</th>
             </tr>
-          ))}
-        </tbody>
+          </thead>
+          <tbody>
+            {filteredData.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.city}</td>
+                <td>{item.occupation}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </table>
     </div>
   )
